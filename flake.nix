9f16f60b3809ce8e3410918e4a2ef6bf700dd1ec
Martin <martin@example.com>
2026-09-07 12:09:02 -0400

    fix: remove noctalia

--- flake.nix
+++ flake.nix
@@ -13,9 +13,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
-    /* Noctalia shell (Wayland desktop shell + launcher) */
-    # noctalia.url = "github:noctalia-dev/noctalia-shell";
-    # noctalia.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
